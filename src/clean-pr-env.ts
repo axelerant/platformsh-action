a011a45a462482c3dd5e7287247465df6a9528b0
zeshanziya <zeshan.ziya@axelerant.com>
2024-06-04 06:24:22 +0000

    add platformsh js

--- src/clean-pr-env.ts
+++ src/clean-pr-env.ts
@@ -1,6 +1,22 @@
 import * as core from '@actions/core'
+import Client from 'platformsh-client'
+
 export async function cleanPrEnv(): Promise<void> {
   core.startGroup('Cleanr PR env from Platform.sh')
+  const client = new Client({
+    api_token: core.getInput('cli-token'),
+    api_url: '',
+    authorization: ''
+  })
+
+  const projects = await client.getProjects()
+  if (projects) {
+    const project = projects[0]
+    core.info(`project name: ${project.name}`)
+  } else {
+    core.info('No projects found')
+  }
+
   core.info('Env cleaned')
   core.endGroup()
 }

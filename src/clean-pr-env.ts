91d0bfb31e4f154a1e358d86ea294ecdf41cf5ce
zeshanziya <zeshan.ziya@axelerant.com>
2024-06-05 06:52:47 +0000

    fetch pr env info

--- src/clean-pr-env.ts
+++ src/clean-pr-env.ts
@@ -1,9 +1,13 @@
 import * as core from '@actions/core'
 import Client from 'platformsh-client'
 import { getAccessToken } from './utils'
+import * as github from '@actions/github'
 
 export async function cleanPrEnv(): Promise<void> {
   core.startGroup('Cleanr PR env from Platform.sh')
+
+  core.info(`PR No: ${github.context.payload.pull_request?.number}`)
+
   const accessToken = await getAccessToken(core.getInput('cli-token'))
   const client = new Client({
     access_token: accessToken,
@@ -11,12 +15,17 @@ export async function cleanPrEnv(): Promise<void> {
     authorization: ''
   })
 
-  const projects = await client.getProjects()
-  if (projects) {
-    const project = projects[0]
-    core.info(`project name: ${project.name}`)
+  // Get env details
+  const prRef = '494/merge'
+  const envResult = await client.getEnvironment(
+    core.getInput('project-id'),
+    encodeURIComponent(prRef)
+  )
+  if (envResult) {
+    core.info(`Enviroment type is ${envResult.type}`)
+    core.info(`Enviroment name is ${envResult.name}`)
   } else {
-    core.info('No projects found')
+    core.info(`No env running for PR - ${prRef}`)
   }
 
   core.info('Env cleaned')

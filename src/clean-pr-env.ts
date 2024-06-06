d49e79d2b63d2a8aa97badd07f7aec04004dd2bb
zeshanziya <zeshan.ziya@axelerant.com>
2024-06-06 06:46:00 +0000

    delete env

--- src/clean-pr-env.ts
+++ src/clean-pr-env.ts
@@ -6,7 +6,13 @@ import * as github from '@actions/github'
 export async function cleanPrEnv(): Promise<void> {
   core.startGroup('Cleanr PR env from Platform.sh')
 
-  core.info(`PR No: ${github.context.payload.pull_request?.number}`)
+  const prNumber = github.context.payload.pull_request?.number
+  if (!prNumber) {
+    core.warning(
+      `Unable to identify PR No. Please make sure this action runs only on PR close`
+    )
+    return
+  }
 
   const accessToken = await getAccessToken(core.getInput('cli-token'))
   const client = new Client({
@@ -16,18 +22,36 @@ export async function cleanPrEnv(): Promise<void> {
   })
 
   // Get env details
-  const prRef = '494/merge'
+  const prRef = `${prNumber}/merge`
   const envResult = await client.getEnvironment(
     core.getInput('project-id'),
     encodeURIComponent(prRef)
   )
-  if (envResult) {
-    core.info(`Enviroment type is ${envResult.type}`)
-    core.info(`Enviroment name is ${envResult.name}`)
-  } else {
-    core.info(`No env running for PR - ${prRef}`)
+
+  core.info(`Environment type is ${envResult.type}`)
+  core.info(`Environment name is ${envResult.name}`)
+
+  if (envResult.type !== 'development') {
+    core.info(
+      `Skipping ${prRef} environment deletion as it's not a development environment`
+    )
+    core.endGroup()
+    return
   }
 
-  core.info('Env cleaned')
+  // Deactivate env first.
+  const activity = await envResult.deactivate()
+  core.info(`Deactivating ${prRef} environment...`)
+  // @todo display activity log
+  await activity.wait()
+  core.info(`${prRef} environment deactivated successfully.`)
+
+  // Fetch again for deletion as active env can not be deleted and current resource points to active env.
+  const envResultDelete = await client.getEnvironment(
+    core.getInput('project-id'),
+    encodeURIComponent(prRef)
+  )
+  await envResultDelete.delete()
+  core.info(`${prRef} environment deleted successfully`)
   core.endGroup()
 }
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import * as core from '@actions/core'
 import { run } from './main'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-try {
-  run()
-} catch (error) {
-  // Fail the workflow run if an error occurs
-  if (error instanceof Error) core.setFailed(error.message)
-}
+
+  run().catch(error => {
+    core.setFailed(error.message)
+  })

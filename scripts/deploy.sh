46d8626631954f2e4a735ad625581d465348bbab
hw <hussainweb@gmail.com>
2025-06-04 09:55:44 -0400

    style: fix indentation in deploy.sh script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 # Set up the private key if we have one.
 if [[ -n "$SSH_PRIVATE_KEY" ]]; then
-  eval "$(ssh-agent -s)"
-  echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+	eval "$(ssh-agent -s)"
+	echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - >/dev/null
 fi
 
 # Copy known hosts into the SSH config.
@@ -14,9 +14,9 @@ platform project:set-remote "${PLATFORM_PROJECT_ID}"
 [[ "$ENVIRONMENT_NAME" == "" ]] && ENVIRONMENT_NAME="$GITHUB_REF_NAME"
 PLATFORM_OPTS="-vv --activate --target $ENVIRONMENT_NAME"
 if [[ -n "$FORCE_PUSH" ]]; then
-  PLATFORM_OPTS="$PLATFORM_OPTS --force"
+	PLATFORM_OPTS="$PLATFORM_OPTS --force"
 fi
 if [[ -n "$PARENT_ENVIRONMENT_NAME" ]]; then
-  PLATFORM_OPTS="$PLATFORM_OPTS --parent $PARENT_ENVIRONMENT_NAME"
+	PLATFORM_OPTS="$PLATFORM_OPTS --parent $PARENT_ENVIRONMENT_NAME"
 fi
 platform push "${PLATFORM_OPTS}"
